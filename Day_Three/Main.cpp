b0d7a216078de31229a271e9346afb8755bb0a69
Edison_SK <81354027+kai0403@users.noreply.github.com>
2023-01-17 20:29:30 +0800

    Add files via upload

---
+++ Day_Three/Main.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:6031)
+#include"Function.h"
+int main()
+{
+	/*char str[] = "A man into a world";
+	string str1 = "Live for being stronger";
+	cout << str << endl;
+	cout << str1 << endl;*/
+	/*bool flag = true;
+	cout << flag << endl;
+	flag = false;
+	cout << flag << endl;
+	cout << sizeof(flag) << endl;*/
+	/*int num1 = 10;
+	cout << "Please input an integral number:" << endl;
+	cin >> num1;
+	cout << "Number1 is " << num1 << endl;*/
+	/*float num2 = 3.14f;
+	cout << "Please input a floating number:" << endl;
+	cin >> num2;
+	cout << "Number2 is " << num2 << endl;*/
+	/*char ch = 'a';
+	cout << "Please input a character:" << endl;
+	cin >> ch;
+	cout << "Character is " << ch << endl;*/
+	string str = "A man into an army";
+	cout << "Please input a string:" << endl;
+	cin >> str;
+	cout << "The string is " << str << endl;
+	system("pause");
+	return 0;
+}
